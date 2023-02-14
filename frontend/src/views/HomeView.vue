eed1478bb1a7462b43bc2c9f26dcc79abc2c786e
sseungki98 <appp_le@naver.com>
2023-02-14 16:47:30 +0900

    chore: Initial settings for project

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+    name: "HomeView",
+    components: {
+        HelloWorld,
+    },
+};
+</script>
+
+<style scoped>
+.home {
+    background-color: black;
+    height: 100vh;
+}
+</style>
