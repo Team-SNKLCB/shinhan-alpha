b1e608399c1d402f6e598a0aa8469ba4ca090637
sseungki98 <appp_le@naver.com>
2023-02-17 18:01:00 +0900

    feat: add mode tutorial view

---
+++ frontend/src/views/ModeTutorialView.vue
@@ -0,0 +1,24 @@
+<template>
+    <phone-header></phone-header>
+    <img class="mode-tutorial" src="../assets/tutorial.png" />
+    <router-link class="admit-btn" to="easy_menu"><img src="../assets/admit-btn.png" /></router-link>
+</template>
+
+<script>
+import PhoneHeader from "@/components/PhoneHeader.vue";
+export default {
+    components: { PhoneHeader },
+};
+</script>
+
+<style>
+.mode-tutorial {
+    width: 320px;
+    margin-top: 40px;
+}
+
+.admit-btn {
+    position: relative;
+    bottom: 145px;
+}
+</style>
