e834e66956cc7a0ba43e33c9e1c16b4409d879f9
sseungki98 <appp_le@naver.com>
2023-02-17 18:03:07 +0900

    chore: setting for project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-createApp(App).use(store).use(router).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faWifi } from "@fortawesome/free-solid-svg-icons";
+import { faBatteryFull } from "@fortawesome/free-solid-svg-icons";
+import { faSignal } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+/* add icons to the library */
+library.add(faWifi, faBatteryFull, faSignal, faBars, faChevronRight);
+
+createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
+import SplashView from "../views/SplashView.vue";
+import ModeSelectView from "../views/ModeSelectView.vue";
+import OrdinaryMenuView from "../views/OrdinaryMenuView.vue";
+import ModeTutorialView from "../views/ModeTutorialView.vue";
+import EasyMenuView from "../views/EasyMenuView.vue";
 const routes = [
     {
         path: "/",
@@ -14,6 +18,31 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    },
+    {
+        path: "/splash",
+        name: "splash",
+        component: SplashView,
+    },
+    {
+        path: "/modeSelect",
+        name: "modeSelect",
+        component: ModeSelectView,
+    },
+    {
+        path: "/ordinary_menu",
+        name: "ordinary_menu",
+        component: OrdinaryMenuView,
+    },
+    {
+        path: "/mode_tutorial",
+        name: "mode_tutorial",
+        component: ModeTutorialView,
+    },
+    {
+        path: "/easy_menu",
+        name: "easy_menu",
+        component: EasyMenuView,
     },
 ];
 
