0e57f649ce44d9b03428dd5d31a0c3eb4f771340
sseungki98 <appp_le@naver.com>
2023-02-17 18:01:40 +0900

    feat: add ordinary menu view

---
+++ frontend/src/views/OrdinaryMenuView.vue
@@ -0,0 +1,24 @@
+<template>
+    <phone-header></phone-header>
+    <img class="ord-menu" src="../assets/ordinary-menu.png" />
+    <bottom-bar></bottom-bar>
+</template>
+
+<script>
+import PhoneHeader from "@/components/PhoneHeader.vue";
+import BottomBar from "@/components/BottomBar.vue";
+export default {
+    components: {
+        PhoneHeader,
+        BottomBar,
+    },
+};
+</script>
+
+<style>
+.ord-menu {
+    margin-top: 40px;
+    margin-bottom: 45px;
+    width: 300px;
+}
+</style>
