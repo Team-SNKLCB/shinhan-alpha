b6bb7f95fe5636b533b9b409d3bb86e32633d635
sseungki98 <appp_le@naver.com>
2023-02-17 18:04:20 +0900

    feat: add easy menu view

---
+++ frontend/src/views/EasyMenuView.vue
@@ -0,0 +1,21 @@
+<template>
+    <phone-header></phone-header>
+    <div class="easy-menu-header"></div>
+</template>
+
+<script>
+import PhoneHeader from "@/components/PhoneHeader.vue";
+export default {
+    components: {
+        PhoneHeader,
+    },
+};
+</script>
+
+<style>
+.easy-menu-header {
+    width: 320px;
+    height: 46px;
+    background-color: #354ef2;
+}
+</style>
