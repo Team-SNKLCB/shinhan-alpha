2209a9cf1acd0034e81d2cf6d22cb7ce21c2e8dc
sseungki98 <appp_le@naver.com>
2023-02-17 18:00:24 +0900

    feat: add splash view

---
+++ frontend/src/views/SplashView.vue
@@ -0,0 +1,17 @@
+<template>
+    <img id="splash" src="../assets/splash.png" />
+</template>
+
+<script>
+import PhoneHeader from "@/components/PhoneHeader.vue";
+export default {
+    components: { PhoneHeader },
+};
+</script>
+
+<style>
+#splash {
+    height: 668px;
+    width: 320px;
+}
+</style>
