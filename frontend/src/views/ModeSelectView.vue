1554528f31404f6df66208c2473f4ccc46e3b269
sseungki98 <appp_le@naver.com>
2023-02-17 18:00:42 +0900

    feat: add mode select view

---
+++ frontend/src/views/ModeSelectView.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="modeSelectBox">
+        <p class="title">어떤 모드를 선택하시겠어요?</p>
+        <router-link to="/about"><img class="btn-mode" src="../assets/button-julin.png" /></router-link>
+        <router-link to="/about"><img class="btn-mode" src="../assets/button-ordinary.png" /></router-link>
+        <p class="sub-title">신한투자증권</p>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.modeSelectBox {
+    height: 100%;
+    background-color: #373deb;
+    padding-top: 131px;
+    box-sizing: border-box;
+}
+.btn-mode {
+    width: 253px;
+    height: 108px;
+    border-radius: 10px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    margin-top: 30px;
+}
+
+p {
+    margin: 0;
+}
+
+.title {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    color: white;
+}
+.sub-title {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 15px;
+    color: white;
+    margin-top: 100px;
+}
+</style>
