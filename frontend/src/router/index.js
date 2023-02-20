8c46257882efaf5c360e056bc8b4aca8d56253fd
HaByeong <jhschris8080@naver.com>
2023-02-20 16:33:08 +0900

    feat: Make TakeRewardView.vue Finish

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,64 +9,65 @@ import MissionView from "../views/MissionView.vue";
 import InviteFriendView from "../views/InviteFriendView.vue";
 import TakeRewardView from "../views/TakeRewardView.vue";
 const routes = [
-    {
-        path: "/about",
-        name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-    },
-    {
-        path: "/splash",
-        name: "splash",
-        component: SplashView,
-    },
-    {
-        path: "/mode_select",
-        name: "mode_select",
-        component: ModeSelectView,
-    },
-    {
-        path: "/ordinary_menu",
-        name: "ordinary_menu",
-        component: OrdinaryMenuView,
-    },
-    {
-        path: "/mode_tutorial",
-        name: "mode_tutorial",
-        component: ModeTutorialView,
-    },
-    {
-        path: "/easy_menu",
-        name: "easy_menu",
-        component: EasyMenuView,
-    },
-    {
-        path: "/tier_main",
-        name: "tier_main",
-        component: TierMainView,
-    },
-    {
-        path: "/mission",
-        name: "misson",
-        component: MissionView,
-    },
-    {
-        path: "/invite",
-        name: "invite",
-        component: InviteFriendView,
-    },
-    {
-        path: "take_reward",
-        name: "take_reward",
-        component: TakeRewardView,
-    },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/splash",
+    name: "splash",
+    component: SplashView,
+  },
+  {
+    path: "/mode_select",
+    name: "mode_select",
+    component: ModeSelectView,
+  },
+  {
+    path: "/ordinary_menu",
+    name: "ordinary_menu",
+    component: OrdinaryMenuView,
+  },
+  {
+    path: "/mode_tutorial",
+    name: "mode_tutorial",
+    component: ModeTutorialView,
+  },
+  {
+    path: "/easy_menu",
+    name: "easy_menu",
+    component: EasyMenuView,
+  },
+  {
+    path: "/tier_main",
+    name: "tier_main",
+    component: TierMainView,
+  },
+  {
+    path: "/mission",
+    name: "misson",
+    component: MissionView,
+  },
+  {
+    path: "/invite",
+    name: "invite",
+    component: InviteFriendView,
+  },
+  {
+    path: "/take_reward",
+    name: "take_reward",
+    component: TakeRewardView,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
