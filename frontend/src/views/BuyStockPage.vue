f44a3d2d2e969a42a6d0c279735a6803808fdd5a
HaByeong <jhschris8080@naver.com>
2023-02-21 21:18:46 +0900

    feat: Make Stock_prize and Stock_num Algorithm

--- frontend/src/views/BuyStockPage.vue
+++ frontend/src/views/BuyStockPage.vue
@@ -15,30 +15,56 @@
           style="width: 200px; position: absolute; right: 0px"
           src="../assets/stock_bottom2.png"
         />
-        <div
-          style="
-            background-color: #f8f8f8;
-            position: absolute;
-            width: 180px;
-            height: 50px;
-            right: 13px;
-            top: 320px;
-            z-index: 3;
-            border-radius: 10px;
-          "
-        ></div>
-        <div
-          style="
-            background-color: #f8f8f8;
-            position: absolute;
-            width: 180px;
-            height: 50px;
-            right: 13px;
-            top: 380px;
-            z-index: 3;
-            border-radius: 10px;
-          "
-        ></div>
+        <div class="up_bar">
+          <img
+            @click="add_stock"
+            style="position: absolute; right: 10px; top: 15px"
+            src="../assets/plus.png"
+          />
+          <img
+            @click="sub_stock"
+            style="position: absolute; left: 10px; top: 15px"
+            src="../assets/minus.png"
+          />
+          <span
+            style="
+              position: absolute;
+              top: 13px;
+              font-size: 18px;
+              font-weight: bold;
+              right: 85px;
+            "
+            >{{ stock_num }} </span
+          ><span
+            style="position: absolute; top: 20px; font-size: 10px; right: 75px"
+            >주</span
+          >
+        </div>
+        <div class="down_bar">
+          <img
+            @click="add_prize"
+            style="position: absolute; right: 10px; top: 15px"
+            src="../assets/plus.png"
+          />
+          <img
+            @click="sub_prize"
+            style="position: absolute; left: 10px; top: 15px"
+            src="../assets/minus.png"
+          />
+          <span
+            style="
+              position: absolute;
+              top: 13px;
+              font-size: 18px;
+              font-weight: bold;
+              right: 60px;
+            "
+            >{{ stock_prize }} </span
+          ><span
+            style="position: absolute; top: 20px; font-size: 10px; right: 50px"
+            >원</span
+          >
+        </div>
       </div>
       <font-awesome-icon
         icon="fa-soild fa-chevron-left"
@@ -53,10 +79,36 @@ import PhoneHeader from "../components/PhoneHeader.vue";
 
 export default {
   components: { PhoneHeader },
+  data() {
+    return {
+      stock_num: 0,
+      stock_sum: 0,
+      stock_prize: 121100,
+    };
+  },
 
   methods: {
     goBack() {
       window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
+    },
+    add_stock() {
+      this.stock_num += 1;
+    },
+    sub_stock() {
+      if (this.stock_num > 0) {
+        this.stock_num -= 1;
+      }
+    },
+    prize_sum() {
+      this.stock_sum = 121100 * this.stock_num;
+    },
+    add_prize() {
+      this.stock_prize += 100;
+    },
+    sub_prize() {
+      if (this.stock_prize > 0) {
+        this.stock_prize -= 100;
+      }
     },
   },
 };
@@ -66,5 +118,25 @@ export default {
   position: absolute;
   left: 9px;
   top: 12px;
+}
+.up_bar {
+  background-color: #f8f8f8;
+  position: absolute;
+  width: 180px;
+  height: 50px;
+  right: 13px;
+  top: 320px;
+  z-index: 3;
+  border-radius: 10px;
+}
+.down_bar {
+  background-color: #f8f8f8;
+  position: absolute;
+  width: 180px;
+  height: 50px;
+  right: 13px;
+  top: 380px;
+  z-index: 3;
+  border-radius: 10px;
 }
 </style>
