feb5fa264497f30db2b80214928885cf64534f89
HaByeong <jhschris8080@naver.com>
2023-02-21 15:44:44 +0900

    feat: Link bronze RewardEvent

--- frontend/src/views/TakeRewardView.vue
+++ frontend/src/views/TakeRewardView.vue
@@ -92,7 +92,9 @@
           style="width: 100%"
         ></div>
       </div>
-      <img class="bronze_star" src="../assets/bronze_star.png" />
+      <router-link to="/reward_event">
+        <img class="bronze_star" src="../assets/bronze_star.png"
+      /></router-link>
       <img class="silver_star" src="../assets/silver_star.png" />
       <img class="gold_star" src="../assets/gold_star.png" />
       <img class="platinum_star" src="../assets/platinum_star.png" />
