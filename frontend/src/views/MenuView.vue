735edf362136f7f50321da99bb629599da01b5c7
sseungki98 <appp_le@naver.com>
2023-02-22 13:24:28 +0900

    feat: merge menu view

---
+++ frontend/src/views/MenuView.vue
@@ -0,0 +1,22 @@
+<template>
+    <easy-menu-view-vue v-if="e_activate === true"></easy-menu-view-vue>
+    <ordinary-menu-view-vue v-if="e_activate === false"></ordinary-menu-view-vue>
+</template>
+
+<script>
+import EasyMenuViewVue from "./EasyMenuView.vue";
+import OrdinaryMenuViewVue from "./OrdinaryMenuView.vue";
+export default {
+    components: {
+        EasyMenuViewVue,
+        OrdinaryMenuViewVue,
+    },
+    computed: {
+        e_activate() {
+            return this.$store.state.e_activate;
+        },
+    },
+};
+</script>
+
+<style></style>
